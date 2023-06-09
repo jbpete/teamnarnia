d1b5c08938405c3553acd7f666c9ddf00f9a4f22
Jack P <jackpeterson99528@gmail.com>
2023-06-08 20:34:38 -0400

    Began adding api fetch and setting up intial home routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,4 +3,17 @@ const userRoutes = require('./userRoutes');
 
 router.use('/users', userRoutes);
 
+apiKey = "7c5341c2e1a248c391cbc63889b96f6f"
+
+function spoonAPI() {
+    let url = `https://api.spoonacular.com/recipes/random?apiKey=7c5341c2e1a248c391cbc63889b96f6f`;
+    fetch(url)
+    .then(response => {
+        return response.json();
+    })
+    //.then()
+}
+
+spoonAPI()
+
 module.exports = router;
