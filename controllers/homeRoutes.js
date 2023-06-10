da21b84ff174f3f0c90e172f8666661aa6f1fdac
Jack P <jackpeterson99528@gmail.com>
2023-06-10 08:55:45 -0400

    Continued adding to API fetch and established more boiler plate route info

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,19 +8,28 @@ router.get('/', withAuth, async (req, res) => {
             attributes: { exclude: ['password'] },
             order: [['name, ASC'],]
         })
+    
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    res.render('homepage', {
+      users,
+      logged_in: req.session.logged_in,
+    });
     } catch (err) {
         res.status(500).json(err);
       }
     });
     
-    router.get('/login', (req, res) => {
-      if (req.session.logged_in) {
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
         res.redirect('/');
         return;
       }
     
       res.render('login');
     });
+
+
     
     module.exports = router;
     
