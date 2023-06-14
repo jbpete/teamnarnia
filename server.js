927f5706dd46983c3ed66a523ea2d94a653fd90e
Heathlington <126424577+Heathlington@users.noreply.github.com>
2023-06-13 19:31:35 -0500

    debugged login route

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers')
 const exphbs = require('express-handlebars');
-
+const path = require('path')
 const hbs = exphbs.create({ helpers });
 
 
@@ -30,6 +30,7 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(routes);
 
